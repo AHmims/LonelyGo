6ca6b4f42e56bf5a082fe70332393110cc883b71
AHmims <ali.hmims99@gmail.com>
2021-03-06 20:20:58 +0100

    conditional-rendering- remember: Capitalcase first letter of class && return in render should be inside a container for exmpl h1 and h2 inside a div

--- src/App.js
+++ src/App.js
@@ -1,75 +1,37 @@
 import React from 'react';
 
-class Counter extends React.Component{
-  constructor(props){
-    super(props);
-    this.state = {
-      counter : 0
-    }
-    //
-    this.click = this.click.bind(this);
-  }
-  //
-  click(e){
-    console.log(e);
-    this.setState((state, props)=>({
-        counter: state.counter + parseInt(props.increment)
-    }));
-  }
+class StateA extends React.Component{
   //
   render(){
-    return (
-      <button onClick={this.click}>Count is : {this.state.counter}</button>
+    return(
+      <h1>STATE A</h1>
     );
   }
 }
-// 
-class Timer extends React.Component{
+
+class StateB extends React.Component{
+  render(){
+    return(
+      <h1>STATE B</h1>
+    );
+  }
+}
+
+class Handler extends React.Component{
   constructor(props){
     super(props);
-    this.state = {
-      date:new Date(),
-      date2:new Date()
-    };
+    this.data = props.state;
   }
-  //
-  componentDidMount(){
-    this.timerId = setInterval(() => {
-      this.tick();
-    }, 1000);
-  }
-  componentWillUnmount(){
-    clearInterval(this.timerId);
-  }
-  componentDidUpdate(){
-    //console.log(this.state);
-  }
-  //
-  tick(){
-    this.setState({
-      date : new Date()
-    });
-    /*this.setState(function(state, props) {
-      return {
-        counter: state.counter + props.increment
-      };
-    });*/
-  }
-  //
+//
   render(){
     return (
       <div>
-        <Counter increment="1"/>
-        <h3>
-          {this.state.date.toLocaleTimeString()}
-        </h3>
-        <h3>
-          {this.state.date2.toLocaleTimeString()}
-        </h3>
+        <h2>test</h2>
+        {(this.data) ? <StateA /> : <StateB />}
       </div>
     );
   }
 }
 //
 
-export default Timer;
+export default Handler;
